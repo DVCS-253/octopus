89540c734d7eca94c737acd0d162daa625a61f11
Nate Hart <nejthan@gmail.com>
2015-10-16 00:57:29 -0400

    Added unit tests for public methods

--- tests/test_pushpull.rb
+++ tests/test_pushpull.rb
@@ -1,10 +1,19 @@
 require 'test/unit'
 
 class TestPushPull < Test::Unit::TestCase
- 
-  # Tests the tentative `apply_differences` method of push/pull
+
+  # Tests pulling from a remote over the network.
   #
-  def test_apply_differences
+  def test_pull
+    # Pull from a test remote on localhost
+    # Assert that the pulled contents are as expected
+  end
+
+  # Tests pushing to a remote over the network.
+  #
+  def test_push
+    # Push to a test remote on localhost
+    # Assert that the contents on the remote are as expected
   end
  
 end
