359fb830d5c7650e8e8f10c7c578f99fe2e6b5ce
Nate Hart <nejthan@gmail.com>
2015-10-15 20:53:42 -0400

    Added initial test outline for push/pull

---
+++ tests/test_pushpull.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+class TestPushPull < Test::Unit::TestCase
+ 
+  # Tests the tentative `apply_differences` method of push/pull
+  #
+  def test_apply_differences
+  end
+ 
+end
