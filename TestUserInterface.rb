002e80883359532dd78b2405bf0d490af751bc4e
Kai-lash <kailashjoshi1104@gmail.com>
2015-11-06 17:28:20 -0500

    categorized the test cases based on the commands

--- TestUserInterface.rb
+++ TestUserInterface.rb
@@ -3,125 +3,143 @@ require_relative "UserInterface"
 
 #Required for unit testing
 require "test/unit"
- 
-#This class contains test cases for UserInterface class
+
+#Contains the test cases for the supported commands
 class TestUserInterface < Test::Unit::TestCase
 
-#Creating an object of UserInterface class
+#Instance of this class
 UI = UserInterface.new()
 
+#--Commands to be tested. 
+#Valid Commands
+InitCmd1 = ['init']
+InitCmd2 = ['init', '"MyDirectory"']
+AddCmd1 = ['add', '"file1"']
+AddCmd2 = ['add', '"file2"']
+AddCmd3 = ['add', '.']
+CheckoutCmd1 = ['checkout']
+CheckoutCmd2 = ['checkout', 'MyBranch']
+CheckoutCmd3 = ['checkout', '-b', 'MyBranch']
+CheckoutCmd4 = ['checkout', 'MyBranch', '--track', 'origin\master']
+CommitCmd1a = ['commit']
+CommitCmd2a = ['commit', '-m', '"added file"']
+CommitCmd3a = ['commit', '-a', '-m', '"added file"']
+CommitCmd4a = ['commit', '-m', '"added file"', 'file1']
+CommitCmd5a = ['commit', '-m', '"added file"', 'file1', 'file2']
+CommitCmd1b = ['commit']
+CommitCmd2b = ['commit', '-m', 'added file']
+CommitCmd3b = ['commit', '-a', '-m', 'added file']
+CommitCmd4b = ['commit', '-m', 'added file', 'file1']
+CommitCmd5b = ['commit', '-m', 'added file', 'file1', 'file2']
+BranchCmd1 = ['branch', '-a']
+BranchCmd2 = ['branch', '-d' 'MyBranch']
+MergeCmd1 = ['merge', 'MyBranch']
+PushCmd1 = ['push', 'origin', 'master']
+PullCmd1 = ['pull', 'origin', 'master']
+StatusCmd1 = ['status']
+CloneCmd1 = ['clone', 'MyRepo']
+CloneCmd2 = ['clone', 'MyRepo', '"MyDirectory"']
+DiffCmd1 = ['diff', 'MyBranch', 'YourBranch']
+HelpCmd1 = ['help']
 
-#List of commands to be tested.Note that I have used 'vcs' as the keyword just as 'git' in GitHub.
-
-C_init = "vcs init"
-C_add = "vcs add"
-C_checkout = "vcs checkout"
-C_commit = "vcs commit"
-C_branch = "vcs branch"
-C_merge = "vcs merge"
-C_push = "vcs push"
-C_pull = "vcs pull"
-C_status = "vcs status"
-C_clone = "vcs clone"
-C_diff = "vcs diff"
-C_help = "vcs help"
-C_invalid_start = "abc commit"
-C_invalid_command = "vcs pulled"
-C_q = "q"
-C_quit = "quit"
-C_end = "end"
-C_exit = "exit"
-
-#List of messages that are shown for each command
-
-Msg_init_success = "'init' executed!"
-Msg_add_success = "'add' executed!"
-Msg_checkout_success = "'checkout' executed!"
-Msg_commit_success = "'commit' executed!"
-Msg_branch_success = "'branch' executed!"
-Msg_merge_success = "'merge' executed!"
-Msg_push_success = "'push' executed!"
-Msg_pull_success = "'pull' executed!"
-Msg_status_success = "'status' executed!"
-Msg_clone_success = "'clone' executed!"
-Msg_diff_success = "'clone' executed!"
-Msg_help_success = "'clone' executed!"
-
-#List of error messages expected during the test
-
-Msg_invalid_start = "Command should start with 'vcs'"
-Msg_invalid_command = "Invalid command "
-
-#List of valid tokens. Token is the array containing a command with its options, if any
-
-C_init_tk = ["init"]
-C_add_tk = ["add"]
-C_checkout_tk = ["checkout"]
-C_commit_tk = ["commit"]
-C_branch_tk = ["branch"]
-C_merge_tk = ["merge"]
-C_push_tk = ["push"]
-C_pull_tk = ["pull"]
-C_status_tk = ["status"]
-C_clone_tk = ["clone"]
-C_diff_tk = ["diff"]
-C_help_tk = ["help"]
-C_invalid_command_tk = ["pulled"]
+#--Invalid Commands
+InitCmdInv = ['init', 'My', 'Directory']
+AddCmdInv = ['add', 'My' 'File']
+CheckoutCmdInv = ['checkout', 'My', 'Branch']
+CommitCmdInv = ['commit', 'My', 'Files']
+BranchCmdInv = ['branch', 'My', 'Directory']
+MergeCmdInv = ['merge', 'Your', 'Directory']
+PushCmdInv = ['push', 'My', 'Repo']
+PullCmdInv = ['pull', 'My' 'Repo']
+StatusCmdInv = ['status', 'My', 'Workspace']
+CloneCmdInv = ['clone', 'My', 'Repo']
+DiffCmdInv = ['diff', 'My', 'Branch', 'and', 'Your', 'Branch']
+HelpCmdInv = ['help', 'Me']
 
 #Test cases
 
-  def test_parseCommand
-	  assert_equal(UI.parseCommand(C_init),UI.main(C_init))
-	  assert_equal(UI.parseCommand(C_add),UI.main(C_add))
-	  assert_equal(UI.parseCommand(C_checkout),UI.main(C_checkout))
-	  assert_equal(UI.parseCommand(C_commit),UI.main(C_commit))
-	  assert_equal(UI.parseCommand(C_branch),UI.main(C_branch))
-	  assert_equal(UI.parseCommand(C_merge),UI.main(C_merge))
-	  assert_equal(UI.parseCommand(C_push),UI.main(C_push))
-	  assert_equal(UI.parseCommand(C_pull),UI.main(C_pull))
-	  assert_equal(UI.parseCommand(C_status),UI.main(C_status))
-	  assert_equal(UI.parseCommand(C_clone),UI.main(C_clone))
-	  assert_equal(UI.parseCommand(C_diff),UI.main(C_diff))
-	  assert_equal(UI.parseCommand(C_help),UI.main(C_help))
-	  assert_equal(UI.parseCommand(C_invalid_start),UI.main(C_invalid_start))
-	  assert_equal(UI.parseCommand(C_invalid_command),UI.main(C_invalid_command))
-	  assert_raise(RuntimeError){ UI.main(C_q) }
-	  assert_raise(RuntimeError){ UI.main(C_quit) }
-	  assert_raise(RuntimeError){ UI.main(C_end) }
-	  assert_raise(RuntimeError){ UI.main(C_exit) }
+  #Test for 'init' command and its parameters
+  def test_init
+  	assert_equal(UI.parseCommand('init', InitCmd1*" "),UI.main(InitCmd1))
+  	assert_equal(UI.parseCommand('init', InitCmd2*" "),UI.main(InitCmd2))
+  	assert_equal(UI.parseCommand('init', InitCmdInv*" "),UI.main(InitCmdInv))
   end
   
-  def test_executeCommand
-      assert_equal(UI.executeCommand(C_init_tk),UI.main(C_init))
-	  assert_equal(UI.executeCommand(C_add_tk),UI.main(C_add))
-	  assert_equal(UI.executeCommand(C_checkout_tk),UI.main(C_checkout))
-	  assert_equal(UI.executeCommand(C_commit_tk),UI.main(C_commit))
-	  assert_equal(UI.executeCommand(C_branch_tk),UI.main(C_branch))
-	  assert_equal(UI.executeCommand(C_merge_tk),UI.main(C_merge))
-	  assert_equal(UI.executeCommand(C_push_tk),UI.main(C_push))
-	  assert_equal(UI.executeCommand(C_pull_tk),UI.main(C_pull))
-	  assert_equal(UI.executeCommand(C_status_tk),UI.main(C_status))
-	  assert_equal(UI.executeCommand(C_clone_tk),UI.main(C_clone))
-	  assert_equal(UI.executeCommand(C_diff_tk),UI.main(C_diff))
-	  assert_equal(UI.executeCommand(C_help_tk),UI.main(C_help))
-	  assert_equal(UI.executeCommand(C_invalid_command_tk),UI.main(C_invalid_command))
+  #Test for 'add' command and its parameters
+  def test_add
+  	assert_equal(UI.parseCommand('add', AddCmd1*" "),UI.main(AddCmd1))
+  	assert_equal(UI.parseCommand('add', AddCmd2*" "),UI.main(AddCmd2))
+  	assert_equal(UI.parseCommand('add', AddCmd3*" "),UI.main(AddCmd3))
+  	assert_equal(UI.parseCommand('add', AddCmdInv*" "),UI.main(AddCmdInv))
   end
   
-  def test_displayResult
-      assert_equal(UI.displayResult(Msg_init_success),UI.main(C_init))
-	  assert_equal(UI.displayResult(Msg_add_success),UI.main(C_add))
-	  assert_equal(UI.displayResult(Msg_checkout_success),UI.main(C_checkout))
-	  assert_equal(UI.displayResult(Msg_commit_success),UI.main(C_commit))
-	  assert_equal(UI.displayResult(Msg_branch_success),UI.main(C_branch))
-	  assert_equal(UI.displayResult(Msg_merge_success),UI.main(C_merge))
-	  assert_equal(UI.displayResult(Msg_push_success),UI.main(C_push))
-	  assert_equal(UI.displayResult(Msg_pull_success),UI.main(C_pull))
-	  assert_equal(UI.displayResult(Msg_status_success),UI.main(C_status))
-	  assert_equal(UI.displayResult(Msg_clone_success),UI.main(C_clone))
-	  assert_equal(UI.displayResult(Msg_status_diff),UI.main(C_diff))
-	  assert_equal(UI.displayResult(Msg_clone_help),UI.main(C_help))
-	  assert_equal(UI.displayResult(Msg_invalid_start),UI.main(C_invalid_start))
-	  assert_match(UI.displayResult(Msg_invalid_command),UI.main(C_invalid_command))
+  #Test for 'checkout' command and its parameters
+  def test_checkout
+  	assert_equal(UI.parseCommand('checkout', CheckoutCmd1*" "),UI.main(CheckoutCmd1))
+  	assert_equal(UI.parseCommand('checkout', CheckoutCmd2*" "),UI.main(CheckoutCmd2))
+  	assert_equal(UI.parseCommand('checkout', CheckoutCmd3*" "),UI.main(CheckoutCmd3))
+  	assert_equal(UI.parseCommand('checkout', CheckoutCmd4*" "),UI.main(CheckoutCmd4))
+  	assert_equal(UI.parseCommand('checkout', CheckoutCmdInv*" "),UI.main(CheckoutCmdInv))
   end
-
+  
+  #Test for 'commit' command and its parameters
+  def test_commit
+  	assert_equal(UI.parseCommand('commit', CommitCmd1a*" "),UI.main(CommitCmd1b))
+  	assert_equal(UI.parseCommand('commit', CommitCmd2a*" "),UI.main(CommitCmd2b))
+  	assert_equal(UI.parseCommand('commit', CommitCmd3a*" "),UI.main(CommitCmd3b))
+  	assert_equal(UI.parseCommand('commit', CommitCmd4a*" "),UI.main(CommitCmd4b))
+  	assert_equal(UI.parseCommand('commit', CommitCmd5a*" "),UI.main(CommitCmd5b))
+  	assert_equal(UI.parseCommand('commit', CommitCmdInv*" "),UI.main(CommitCmdInv))
+  end
+  
+  #Test for 'branch' command and its parameters
+  def test_branch
+  	assert_equal(UI.parseCommand('branch', BranchCmd1*" "),UI.main(BranchCmd1))
+  	assert_equal(UI.parseCommand('branch', BranchCmd2*" "),UI.main(BranchCmd2))
+  	assert_equal(UI.parseCommand('branch', BranchCmdInv*" "),UI.main(BranchCmdInv))
+  end
+  
+  #Test for 'merge' command and its parameters
+  def test_merge
+  	assert_equal(UI.parseCommand('merge', MergeCmd1*" "),UI.main(MergeCmd1))
+  	assert_equal(UI.parseCommand('merge', MergeCmdInv*" "),UI.main(MergeCmdInv))
+  end
+  
+  #Test for 'push' command and its parameters
+  def test_push
+  	assert_equal(UI.parseCommand('push', PushCmd1*" "),UI.main(PushCmd1))
+  	assert_equal(UI.parseCommand('push', PushCmdInv*" "),UI.main(PushCmdInv))
+  end
+  
+  #Test for 'pull' command and its parameters
+  def test_pull
+  	assert_equal(UI.parseCommand('pull', PullCmd1*" "),UI.main(PullCmd1))
+  	assert_equal(UI.parseCommand('pull', PullCmdInv*" "),UI.main(PullCmdInv))
+  end
+  
+  #Test for 'status' command and its parameters
+  def test_status
+  	assert_equal(UI.parseCommand('status', StatusCmd1*" "),UI.main(StatusCmd1))
+  	assert_equal(UI.parseCommand('status', StatusCmdInv*" "),UI.main(StatusCmdInv))
+  end
+  
+  #Test for 'clone' command and its parameters
+  def test_clone
+  	assert_equal(UI.parseCommand('clone', CloneCmd1*" "),UI.main(CloneCmd1))
+  	assert_equal(UI.parseCommand('clone', CloneCmd2*" "),UI.main(CloneCmd2))
+  	assert_equal(UI.parseCommand('clone', CloneCmdInv*" "),UI.main(CloneCmdInv))
+  end
+  
+  #Test for 'diff' command and its parameters
+  def test_diff
+  	assert_equal(UI.parseCommand('diff', DiffCmd1*" "),UI.main(DiffCmd1))
+  	assert_equal(UI.parseCommand('diff', DiffCmdInv*" "),UI.main(DiffCmdInv))
+  end
+  
+  #Test for 'help' command and its parameters
+  def test_help
+  	assert_equal(UI.parseCommand('help', HelpCmd1*" "),UI.main(HelpCmd1))
+  	assert_equal(UI.parseCommand('help', HelpCmdInv*" "),UI.main(HelpCmdInv))
+  end
+  
 end
