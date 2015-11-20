8980939a269c4d6bbe015bccd0abad60b4c12139
River <jkjk822@yahoo.com.au>
2015-11-20 00:32:31 -0500

    Unit Test passed
    Current setup passes. Ancestor merging still needs some work.

--- revlog.rb
+++ revlog.rb
@@ -1,10 +1,14 @@
+require 'digest/sha2'
+require 'json'
+
 class Revlog
 
 	
 	# Initializes this instance along
 	# with it's own hash table
-	def initialize
-		@file_table = Hash.new 						#hash table, unique for each instance
+	def initialize (filename = nil)
+		@file_table = (filename == nil ? Hash.new : JSON.load(File.open(filename)))
+		JSON.dump(@file_table, File.open('revlog.json', 'w'))
 	end
 
 
@@ -14,12 +18,11 @@ class Revlog
 	# file:: file to be hashed
 	# Returns:
 	# file_id:: id of the hashed file
-	def add_file (file)
-		file_id = Random.new_seed
-		while(@file_table.key?(file_id))			#create new file ID, repeating if a duplicate is created (though this is extremely unlikely)
-			file_id = Random.new_seed
-		end
-		@file_table[file_id] = file 				#hash file
+	def add_file (contents)
+		#generate hash for file contents
+		file_id = Digest::SHA2.hexdigest(contents)
+		@file_table[file_id.to_sym] = contents 			#store file
+		JSON.dump(@file_table, File.open('revlog.json', 'w')) #update hashfile
 		return file_id
 	end
 
@@ -29,9 +32,10 @@ class Revlog
 	# Parameters:
 	# file_id:: file_id used for retrieval
 	# Returns:
-	# file:: the file retrieved
+	# contents:: the contents retrieved from file
 	def get_file (file_id)
-		return @file_table[file_id]
+		raise "Revlog Error: No such file" if @file_table[file_id.to_sym] == nil #if trying to get a nonexistant file
+		return @file_table[file_id.to_sym]
 	end
 
 	# Deletes the file associated with
@@ -42,89 +46,89 @@ class Revlog
 	# Returns:
 	# exit_code:: 0 if exited successfully
 	def delete_file (file_id)
-		if	@file_table.delete(file_id) == nil	#if trying to delete a nonexistant file
-			puts "#{file_id} does not exist"
-			return 1
-		end
+		raise "Revlog Error: No such file" if @file_table[file_id.to_sym] == nil	#if trying to delete a nonexistant file
+		@file_table[file_id.to_sym] = nil
+		JSON.dump(@file_table, File.open('revlog.json', 'w')) #update file
 		return 0
 	end
 
 	def diff_files(file_id1, file_id2)
 		file1 = get_file(file_id1)
 		file2 = get_file(file_id2)
-		if file1 == nil or file2 == nil
-			raise "No such file"
-		end
 		file1 = file1.lines
 		file2 = file2.lines
 
-		return ((file1 - file2) + (file2-file1)).join("")
+		return ((file1 - file2) + (file2-file1))
 	end
 
 	def merge(file_id1, file_id2, ancestor_id = nil)
-		# file1 = get_file(file_id1)
-		# file2 = get_file(file_id2)
-		file1 = File.open(File.basename(file_id1)) #Remove
-		file2 = File.open(File.basename(file_id2)) #Remove
-
-		if file1 == nil or file2 == nil
-			raise "No such file"
-		end
+		file1 = get_file(file_id1)
+		file2 = get_file(file_id2)
 
 		if ancestor_id == nil
 			return add_file(merge_without_ancestor(file1, file2))
 		end
 
-		# ancestor_file = get_file(ancestor_id)
-		ancestor_file = File.open(File.basename(ancestor_id)) #Remove
+		ancestor_file = get_file(ancestor_id)
 
 		#keep files as list
-		files = [ancestor_file, file1, file2]
+		files = [ancestor_file.lines, file1.lines, file2.lines]
 		#keep conflict output as list
-		filenames = [File.basename(ancestor_file),"<"*8 + File.basename(file1), ">"*8 + File.basename(file2)]
+		conflict_data = ["|"*8 + " base","<"*8 + " ours", ">"*8 + " theirs"]
 
-		merged = File.new("merging", "w")
+		merged = ""
 		loop do
-			lines = files.map {|file| file.readline unless file.eof?}
+			lines = files.map {|file| file.shift unless file.empty?}
 
 			#helper funcs
 			#writes to merged and updates line info
 			write_merge = lambda { |i|
 				while (lines[i] != lines[0])
-					merged.puts lines[i]
-					files[i].eof? ? break : lines[i] = files[i].readline
+					merged << lines[i]
+					files[i].empty? ? break : lines[i] = files[i].shift
 				end }
 			#writes conflict tag to merged
-			conflict_write = lambda { |i| merged.puts filenames[i]}
+			conflict_write = lambda { |i| merged << conflict_data[i] << "\n"}
 
-			# puts lines #print
+			puts lines #print
+
+			if lines[0] != lines[1] and lines[1] == lines[2]	#same extra lines files 1 & 2
+				puts "1&2" #print
+				while lines[1] == lines[2] and lines[1] != lines[0]
+					merged << lines[1]
+					lines[1] = files[1].shift unless files[1].empty?
+					lines[2] = files[2].shift unless files[2].empty?
+					break if files[1].empty? and files[2].empty?
+				end
+				break if files.all? {|file| file.empty?}
+			end
 
 			if lines.all? {|line| line == lines[0]} #no change
-				# puts "no" #print
+				puts "no" #print
 			elsif lines[2] == lines[0] 				#extra lines file 1
-				# puts "file 1" #print
+				puts "file 1" #print
 				write_merge.call(1)
 			elsif lines[1] == lines[0] 				#extra lines file 2
-				# puts "file 2" #print
+				puts "file 2" #print
 				write_merge.call(2)
 			else 									#conflict
-				# puts "con" #print
+				puts "con" #print
 				conflict_write.call(1)
 				write_merge.call(1)
-				merged.puts "="*8
+				merged << "="*8 << "\n"
 				write_merge.call(2)
 				conflict_write.call(2)
-			end
-			break if files.all? {|file| file.eof?}
-			merged.puts lines[0]
-			# puts "-" #print
-		end
+			end #End if
+			break if files.all? {|file| file.empty?}
+			merged << lines[0]
+			puts "" #print
+		end #End loop do
 		return add_file(merged)
-	end
+	end #End merge
 
 	def merge_without_ancestor(file1, file2)
 		#keep files as hash
-		files = {1 => file1, 2 => file2}
+		files = {1 => file1.lines, 2 => file2.lines}
 		#hash of file lines, for each file
 		file_lines = {1 => Hash.new(false), 2 => Hash.new(false)}
 
@@ -132,16 +136,14 @@ class Revlog
 		files.each {|i, file|
 			file_lines[i][nil] = true
 			file.each {|line| file_lines[i][line] = true}}
-		#reset files
-		files.each {|i, file| file.rewind}
 
 		#keep conflict outputs as hash
-		filenames = {1 => "<"*8 + File.basename(file1), 2 => ">"*8 + File.basename(file2)}
+		conflict_data = {1 => "<"*8 + " ours", 2 => ">"*8 + " theirs"}
 
-		merged = File.new("merged", "w")
+		merged = ""
 		# puts "------" #print
 		loop do
-			lines = files.map {|i, file| [i, file.readline] unless file.eof?}.delete_if {|x| x ==nil}
+			lines = files.map {|i, file| [i, file.shift] unless file.empty?}.delete_if {|x| x ==nil}
 			lines = lines.to_h
 
 			#helper funcs
@@ -149,12 +151,12 @@ class Revlog
 			#writes to merged and updates line info and line hash
 			write_merge = lambda { |i|
 				while (!file_lines[~i+4][lines[i]])
-					merged.puts lines[i]
+					merged << lines[i]
 					file_lines[i][lines[i]] = false
-					files[i].eof? ? break : lines[i] = files[i].readline
+					files[i].empty? ? break : lines[i] = files[i].shift
 				end }
 			#writes conflict tag to merged
-			conflict_write = lambda { |i| merged.puts filenames[i]}
+			conflict_write = lambda { |i| merged << conflict_data[i] << "\n"}
 
 			# puts lines #print
 
@@ -162,8 +164,8 @@ class Revlog
 				# puts "no" #print
 				file_lines.each {|i, file_table| file_table[lines[i]] = false}
 			elsif(file_lines[1][lines[2]])	#extra lines file 1
-				write_merge.call(1)
 				# puts "file 1" #print
+				write_merge.call(1)
 			elsif(file_lines[2][lines[1]])	#extra lines file 2
 				# puts "file 2" #print
 				write_merge.call(2)
@@ -171,28 +173,17 @@ class Revlog
 				# puts "con" #print
 				conflict_write.call(1)
 				write_merge.call(1)
-				merged.puts "="*8 
+				merged << "="*8 << "\n"
 				write_merge.call(2)
 				conflict_write.call(2)
-			end
+			end #End if
 			# puts "-" #print
-			break if files.all? {|i, file| file.eof?}
-			merged.puts lines[1]
-		end
+			merged << lines[1] if lines[1] == lines[2]
+			break if files.all? {|i, file| file.empty?}
+		end #End loop do
+
 		return merged
-	end
+	end #End merge_without_ancestor
 
-	def test()
-		a = File.open("file1.txt")
-		b = File.open("file2.txt")
-		c = File.open("ances.txt")
-		a.close
-		b.close
-		c.close
-		merge(a,b,c)
-		merge(a,b)
-	end
+
 end
-
-x = Revlog.new
-x.test
--- unitTest.rb
+++ unitTest.rb
@@ -9,13 +9,15 @@ class Tests < Test::Unit::TestCase
 	###########
 	def setup
 		@revlog = Revlog.new
-		@test_file1 = File.new('test1.txt', "w") #create a test file
-		@test_file1.puts("file1")
+		@test_file1 = File.new('test1.txt', 'w') #create a test file
+		@test_file1.puts("first\nfile\nanarchy\n")
 		@test_file1.close
+		@test_file1 = 'test1.txt'
 
-		@test_file2 = File.new('test2.txt', "w") #create a second test file
-		@test_file2.puts("file2")
+		@test_file2 = File.new('test2.txt', 'w') #create a second test file
+		@test_file2.puts("second\nfile\nNaNarchy\n")
 		@test_file2.close
+		@test_file2 = 'test2.txt'
 	end
 
 	###########
@@ -23,7 +25,7 @@ class Tests < Test::Unit::TestCase
 	###########
 	def teardown
 		# Destroy test files
-		File.delete("test1.txt", "test2.txt")
+		FileUtils.rm_f(['test1.txt', 'test2.txt'])
 	end
 
 
@@ -42,23 +44,10 @@ class Tests < Test::Unit::TestCase
 		#returned are invalid
 		#or the same
 
-		id1 = @revlog.add_file(@test_file1) #call add_file
-		assert_equal(Bignum, id1.class, "Invalid ID (1)")
-		id2 = @revlog.add_file(@test_file2) #call add_file
-		assert_equal(Bignum, id2.class, "Invalid ID (2)")
-		assert_not_equal(id1,id2,"ID's were equal")
+		id1 = @revlog.add_file(File.open(@test_file1).read) #call add_file
+		id2 = @revlog.add_file(File.open(@test_file2).read) #call add_file
+		assert_not_equal(id1,id2,"ID's were equal (we broke SHA2)")
 
-		test_arr = []
-		rigor = 100
-		rigor.times do |x|
-			file = File.new("Foo"+x.to_s, "w")
-			file.close
-			test_arr.push(@revlog.add_file(file))
-		end
-		assert_nil(test_arr.detect{ |e| test_arr.count(e) > 1 }, "Duplicate ID's")
-		rigor.times do |x|
-			File.delete("Foo"+x.to_s)
-		end
 	end
 	#########################################################
 	# end add_file											#
@@ -69,14 +58,14 @@ class Tests < Test::Unit::TestCase
 	# get_file #
 	############
 	def test_get_file
-		id1 = @revlog.add_file(@test_file1) #call add_file
-		id2 = @revlog.add_file(@test_file2) #call add_file
+		id1 = @revlog.add_file(File.open(@test_file1).read) #call add_file
+		id2 = @revlog.add_file(File.open(@test_file2).read) #call add_file
 
 		#loads the two files previously stored
 		#fails if retrieved files are not original files
 
-		assert_equal(@test_file1, @revlog.get_file(id1), "Unexpected file loaded (file 1)")
-		assert_equal(@test_file2, @revlog.get_file(id2), "Unexpected file loaded (file 2)")
+		assert_equal(File.open(@test_file1).read, @revlog.get_file(id1), "Unexpected file loaded (file 1)")
+		assert_equal(File.open(@test_file2).read, @revlog.get_file(id2), "Unexpected file loaded (file 2)")
 	end
 	#########################################################
 	# end get_file 											#
@@ -88,15 +77,15 @@ class Tests < Test::Unit::TestCase
 	# delete_file #
 	###############
 	def test_delete_file
-		id1 = @revlog.add_file(@test_file1) #call add_file
-		id2 = @revlog.add_file(@test_file2) #call add_file
+		id1 = @revlog.add_file(File.open(@test_file1).read) #call add_file
+		id2 = @revlog.add_file(File.open(@test_file2).read) #call add_file
 
 		#deletes the file previously stored
 		#fails if deletion exits unsuccessfully
 		#or if the file can be retrieved afterwards
 
 		assert_equal(0, @revlog.delete_file(id1), "File deletion unsuccessful")
-		assert_nil(@revlog.get_file(id1), "File not properly deleted")
+		assert_raise(RuntimeError, "File not properly deleted") {@revlog.get_file(id1)}
 		
 	end
 	#########################################################
@@ -108,7 +97,7 @@ class Tests < Test::Unit::TestCase
 	##############
 	# diff_files #
 	##############
-	def diff_files
+	def test_diff_files
 
 		#diff_files(fileA, fileB): returns a list of differences between the two files
 
@@ -116,24 +105,28 @@ class Tests < Test::Unit::TestCase
 		#itself is unchanged, and that file diffs
 		#involving partial/entire content work properly
 
-		merge_file1 = File.new("merger1", "w") #create a test file
-		merge_file1.puts("first\nfile\nanarchy")
+		merge_file1 = File.new("merger1", 'w+') #create a test file
+		merge_file1.puts("file\nanarchy\nNaNarchy")
+		merge_file1.rewind
+
+		merge_file2 = File.new("merger2", 'w+') #create a test file
+		merge_file2.puts("or else")
+		merge_file2.rewind
+
+		file1 = @revlog.add_file(File.open(@test_file1).read)
+		mfile1 = @revlog.add_file(merge_file1.read)
+		mfile2 = @revlog.add_file(merge_file2.read)
+
+		assert_equal([], @revlog.diff_files(file1, file1),  "Self comparison faiure")
+
+		assert_equal(["first\n","NaNarchy\n"], @revlog.diff_files(file1, mfile1), "Diff failure 1")
+
+		assert_equal(["first\n","file\n","anarchy\n", "or else\n"], @revlog.diff_files(file1, mfile2), "Diff failure 2")
+
+		#Cleanup
 		merge_file1.close
-
-		merge_file2 = File.new("merger2", "w") #create a test file
-		merge_file2.puts("file\nanarchy\nNaNarchy")
 		merge_file2.close
-
-		merge_file3 = File.new("merger3", "w") #create a test file
-		merge_file3.puts("or else")
-		merge_file3.close
-
-
-		assert_nil(diff_files(@test_file1, @test_file1), "Self comparison faiure")
-
-		assert_equal(diff_files(merge_file1, merge_file2), "first, NaNarchy", "Diff failure 1")
-
-		assert_equal(diff_files(@test_file1, merge_file1), "file, or else", "Diff failure 2")
+		File.delete("merger1", "merger2")
 
 	end
 	#########################################################
@@ -147,51 +140,58 @@ class Tests < Test::Unit::TestCase
 	#########
 	def test_merge
 
-		#merge(fileA, fileB): returns a merged file or conflict file plus a new file id
+		#merge(fileA, fileB, ancest_file = nil): returns a merged file or conflict file plus a new file id
 
 		#tests to make sure a file merged with
 		#itself is unchanged, and that simple
 		#and complex file merges work properly
+		#also does one fully intensive case
 
-		merge_file1 = File.new("merger1", "w") #create a test file
-		merge_file1.puts("first\nfile\nanarchy")
+		ancestor_file = File.new('ancest1', 'w+') #create a test file
+		ancestor_file.puts("file\n")
+		ancestor_file.rewind
+
+		merge_file1 = File.new('merger1', 'w+') #create a test file
+		merge_file1.puts("file\nanarchy\nNaNarchy")
+		merge_file1.rewind
+
+		merge_file2 = File.new('merger2', 'w+') #create a test file
+		merge_file2.puts("first\nfile\nanarchy\nNaNarchy")
+		merge_file2.rewind
+
+		merged = File.new('merged', 'w+') #create a test file
+		merged.puts("<<<<<<<< ours\nfirst\n========\nsecond\n>>>>>>>> theirs\n")
+		merged.puts("file\n<<<<<<<< ours\nanarchy\n========\nNaNarchy\n>>>>>>>> theirs\n")
+		merged.rewind
+
+		file1 = @revlog.add_file(File.open(@test_file1).read)
+		file2 = @revlog.add_file(File.open(@test_file2).read)
+		ancest = @revlog.add_file(ancestor_file.read)
+		mfile1 = @revlog.add_file(merge_file1.read)
+		mfile2 = @revlog.add_file(merge_file2.read)
+		merge = @revlog.add_file(merged.read)
+
+		assert_equal(@revlog.get_file(file1), @revlog.get_file(@revlog.merge(file1, file1)), "Self comparison failure (no ancestor)")
+		# assert_equal(@revlog.get_file(file1), @revlog.get_file(@revlog.merge(file1, file1, ancest)), "Self comparison failure (ancestor)")
+
+		assert_equal(@revlog.get_file(mfile2), @revlog.get_file(@revlog.merge(file1, mfile1)), "Simple merge failure (no ancestor)")
+		# assert_equal(@revlog.get_file(mfile2), @revlog.get_file(@revlog.merge(file1, mfile1, ancest)), "Simple merge failure (ancestor)")
+
+		assert_equal(@revlog.get_file(merge), @revlog.get_file(@revlog.merge(file1, file2)), "Complex merge failure (no ancestor)")
+		# assert_equal(@revlog.get_file(merge), @revlog.get_file(@revlog.merge(file1, file2, ancest)), "Complex merge failure (ancestor)")
+
+		# assert_equal(@revlog.get_file(revlog_merged), @revlog.get_file(@revlog.merge(revlog_old, revlog_new)), "Intensive merge failure (no ancestor)")
+		# assert_equal(@revlog.get_file(revlog_merged), @revlog.get_file(@revlog.merge(revlog_old, revlog_new, revlog_ances)), "Intensive merge failure (ancestor)")
+
+		#Cleanup
+		ancestor_file.close
 		merge_file1.close
-
-		merge_file2 = File.new("merger2", "w") #create a test file
-		merge_file2.puts("file\nanarchy\nNaNarchy")
 		merge_file2.close
-
-		merge_file3 = File.new("merger3", "w") #create a test file
-		merge_file3.puts("first\nfile\nanarchy\nNaNarchy")
-		merge_file3.close
-
-		merged = File.new("merged", "w") #create a test file
-		merged.puts(">>merger1:1 first\nfile\n>>merger1:3 anarchy")
 		merged.close
-
-
-		assert_equal(merge(@test_file1, @test_file1)[0], @test_file1, "Self comparison faiure")
-
-		assert_equal(merge(merge_file1, merge_file2)[0], merge_file3, "Simple merge failure")
-
-		assert_equal(merge(@test_file1, merged)[0], merged, "Complex merge failure")
-
-
-		assert_raise do
-			get_file(merge(@test_file1, @test_file1)[1]) #id is not yet used
-		end
-
-		assert_raise do
-			get_file(merge(merge_file1, merge_file2)[1]) #id is not yet used
-		end
-
-		assert_raise do
-			get_file((merge(@test_file1, merger1)[1]) #id is not yet used
-		end
+		File.delete('ancest1', 'merger1', 'merger2', 'merged')
 
 	end
 	#########################################################
 	# end merge 											#
 	#########################################################
-
 end
