792b58f34a0daa7b692d234025e5de58b044e09a
Jamie Alexander <jalexa17@u.rochester.edu>
2015-11-21 17:31:21 -0500

    Testing inter-module param passing

--- Implementation/Intergration_Tests.rb
+++ Implementation/Intergration_Tests.rb
@@ -1,13 +1,18 @@
 require "test/unit"
 
 #Modules 
+require_relative 'PushPull'
 require_relative 'RevLog'
-#require_relative 'Repos'
+require_relative 'Repos'
 require_relative 'UserInterface'
 require_relative 'Workspace'
-#require_relative 'PushPull'
 
 
+## This currently checks to make sure that all modules run, can see eachother
+##      and pass the correct paramters and return the correct parameters when using
+## =>   inter-module function calls. 
+## Once this works, we will focus on makeing sure that the correctness of each function
+## =>   is 100% correct. I.e. that push updates the correct files to the correct branch
 
 class IntergrationTest< Test::Unit::TestCase
 
@@ -46,26 +51,29 @@ class IntergrationTest< Test::Unit::TestCase
     end
 
     def test_PushPull_from_Repos(cmd, params)
-
+        #Can send a snapshot to add to a branch
     end
 
-    def test_Revlog_to_Repos(cmd, params)
+    def test_PushPull_to_Repos(cmd, params)
+        #Can send a merge request and snapshot to merge two snapshots
+    end
 
+    def test_Repos_from_Revlog(cmd, params)
+        #Repos recieves contents of committed files and gets file ID
     end
 
     def test_PushPull_from_Workspace(cmd, params)
-    
+        #PushPull recieves latest snapshot ID from Workspace
     end
 end
 
 
 #Current Test
 def test_UI_to_Workspace(cmd, params)
+     w = Workspace.new()
+
     if cmd.eql? "checkout"
-        Workspace.checkout(params)
-      #  puts "Ran"
-    else
-      #  puts "didnt run"
+        w.check_out(params)
     end
 
     if cmd.equal? "commit"
@@ -79,6 +87,11 @@ end
 
 test_hash = Hash.new
 test_hash[:branch] = "Jamie_Dev"
-
 test_UI_to_Workspace("checkout",test_hash)
+
+test_hash = Hash.new
+test_hash[:file1] = "filename.dat"
+test_UI_to_Workspace("commit",nil)
+test_UI_to_Workspace("commit",test_hash)
+
 
