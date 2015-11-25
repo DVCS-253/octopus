73291b89c5c3b49ea2dcef36fc79e14731eede7e
Tianran Hu <thu@cycle1.cs.rochester.edu>
2015-11-24 20:23:21 -0500

    update test cases

--- workspace/workspace_testcases.rb
+++ workspace/workspace_testcases.rb
@@ -1,100 +1,129 @@
 require "test/unit"
-require_relative "workspace"
+require_relative "workspace_testversion"
 
 
 class DVCS_test < Test::Unit::TestCase
 	my_workspace = Workspace.new()
 
 
-	def test_checkout_snapshot()
-		flag = true
-		workspace = '.octopus/'
+	def test_init
 		my_workspace = Workspace.new()
+		my_workspace.init
+		my_workspace.clean
+		assert_equal(true, File.directory?())
+		assert_equal(true, File.exist?())
+		assert_equal(true, File.exist?())
+		assert_equal(true, File.exist?())
+	end
+
+
+
+	def test_rebuild_dir
+		path = 'Test/test.rb'
+		my_workspace = Workspace.new()
+		my_workspace.init
+		my_workspace.clean
+		my_workspace.rebuild_dir(path)
+		assert_equal(true, File.directory?('./Test'))
+		assert_equal(false, File.exist?('./Test/test.rb'))
+	end
+
+
+	def test_clean
+		my_workspace = Workspace.new()
+		my_workspace.init
+		File.write('test,rb', 'test_content')
+		assert_equal(false, File.exist?('test.rb'))
+		assert_equal(true, File.directory?())
+		assert_equal(true, File.exist?())
+		assert_equal(true, File.exist?())
+		assert_equal(true, File.exist?())
+		
+	end
+
+
+	def test_checkout_snapshot
+		flag = true
+		my_workspace = Workspace.new()
+		my_workspace.init
+		my_workspace.clean
 		my_workspace.check_out_snapshot(1)
 		file_lst = ['test1.txt', 'test2.txt', 'text3.txt']
 		file_lst.each do |f|
-			flag = flag & File.exist?(workspace + f)
+			flag = flag & File.exist?(f)
 		end
 		assert_equal(true, flag, 'Errors, not all files are copied to workspace!')
 	end
 
 
 
-	def test_clean()
-		workspace = '.octopus/'
+	def test_checkout_file
 		my_workspace = Workspace.new()
-		File.write(workspace + 'test.txt', '1')
-		my_workspace.clean()
-		flag = true
-		flag = flag & File.exist?(workspace + 'test.txt')
-		assert_equal(false, flag, 'Errors, files are not deleted!')
+		my_workspace.init
+		my_workspace.clean
+		my_workspace.check_out_file(1)
+		file_name = 'test1.txt'
+		flag = File.exist?(file_name)
+		assert_equal(true, flag, 'Errors, not all files are copied to workspace!')
+	end
+
+
+
+	def test_checkout
+		test_checkout_file
+		test_checkout_snapshot
+	end
+
+	
+	def test_build_hash
+		my_workspace = Workspace.new()
+		my_workspace.init
+		my_workspace.clean
+		File.write('test1.rb', '1')
+		File.write('test2.rb', '2')
+		File.write('tset3.rb', '3')
 		file_lst = ['test1.txt', 'test2.txt', 'text3.txt']
-		file_lst.each do |f|
-			flag = flag & File.exist?(workspace + f)
-		end
-		assert_equal(true, flag, 'Errors, not all files are copied to workspace!')
-	end
+		hash = my_workspace.build_hash(file_lst)
+		corret = {"test2.txt"=>'1', "text2.txt"=>2, "test3.txt"=>3}
+		assert_equal(corret, hash)
+	
 
-
-
+	
 	def test_commit_nil()
-		workspace = '.octopus/'
 		my_workspace = Workspace.new()
-		my_workspace.init()
-		File.write(workspace + 'test1.txt', '1')
-		File.write(workspace + 'test2.txt', '1')
-		File.write(workspace + 'test3.txt', '1')
-		file_hash = my_workspace.commit()
-		answer = {".octopus/test2.txt"=>1, ".octopus/text3.txt"=>1, ".octopus/test1.txt"=>1}
+		my_workspace.init
+		my_workspace.clean
+		File.write('test1.txt', '1')
+		File.write('test2.txt', '1')
+		File.write('test3.txt', '1')
+		file_hash = my_workspace.commit(commit_msg = 'test')
+		answer = {"test2.txt"=>1, "text3.txt"=>1, "test1.txt"=>1}
 		assert_equal(file_hash, answer, 'Errors, incorrect file hash table with nil parameter')	
 	end
 
 
 
 	def test_commit_files()
-		workspace = '.octopus/'
 		my_workspace = Workspace.new()
-		my_workspace.init()
-		File.write(workspace + 'test1.txt', '1')
-		File.write(workspace + 'test2.txt', '1')
-		file_hash = my_workspace.commit([workspace + 'test1.txt', workspace + 'test2.txt'])
-		answer = {".octopus/test1.txt"=>1, ".octopus/test2.txt"=>1, ".octopus/Test1.txt"=>2, ".octopus/Test2.txt"=>3, ".octopus/Text3.txt"=>nil}
+		my_workspace.init
+		my_workspace.clean
+		File.write('test1.txt', '1')
+		File.write('test2.txt', '1')
+		file_hash = my_workspace.commit(['test1.txt', 'test2.txt'], commit_msg = 'test')
+		answer = {"test1.txt"=>'1', "test2.txt"=>'1'}
 		assert_equal(file_hash, answer, 'Errors, incorrect file hash table with files')	
 	end
 
 
+	def test_commit_files()
+		my_workspace = Workspace.new
+		my_workspace.init
+		my_workspace.clean
+		File.write('test1.txt', '1')
+		File.write('test2.txt', '2')
+		File.write('test3.txt', '3')
+		correct = ['test3.txt']
+		uncommitted = my_workspace.status
+		assert_equal(uncommitted, correct)	
 
-	def test_commit_file()
-		workspace = '.octopus/'
-		my_workspace = Workspace.new()
-		my_workspace.init()
-		File.write(workspace + 'test1.txt', '1')
-		file_hash = my_workspace.commit([workspace + 'test1.txt', workspace + 'test2.txt'])
-		answer = {".octopus/test1.txt"=>1, ".octopus/Test1.txt"=>2, ".octopus/Test2.txt"=>3, ".octopus/Text3.txt"=>nil}
-		assert_equal(file_hash, answer, 'Errors, incorrect file hash table with one single file')	
-	end
-
-
-
-	def test_status()
-		workspace = '.octopus/'
-		my_workspace = Workspace.new()
-		my_workspace.init()
-		my_workspace.check_out_snapshot(1)
-		add, delete, update, rename = my_workspace.status()
-		assert_equal(add, [".octopus/test3.txt"], 'Errors, new added file list is incorrect.')
-		assert_equal(delete, [".octopus/Test1.txt"], 'Errors, deleted file list is incorrect.')
-		assert_equal(update, [".octopus/test2.txt"], 'Errors, updated files list is incorrect.')
-		assert_equal(rename,  [".octopus/Test3.txt => .octopus/test1.txt"], 'Errors, renamed file list is incorrect.')
-	end
-
-
-	def test_share_value()
-		my_workspace = Workspace.new()
-		test_hash = {'one' => 1, 'two' => 2, 'three' => 3}
-		answer = my_workspace.share_value(test_hash, 1)
-		assert_equal(answer, 'one')
-		answer = my_workspace.share_value(test_hash, 4)
-		assert_equal(answer, false)
-	end
 end
